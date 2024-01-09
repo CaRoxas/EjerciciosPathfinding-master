7599fc280186003b0e58f7c5586b191ba19b0ab3
CaRoxas <carlotacarballolol@gmail.com>
2024-01-09 16:15:58 +0000

    Dia 09/01
    Ejercicio 3: Hacer que el vigilante se mueva por distintos puntos del plano

--- Assets/Scripts/EjercicioTres/MovimientoPuntos.cs
+++ Assets/Scripts/EjercicioTres/MovimientoPuntos.cs
@@ -5,27 +5,39 @@ using UnityEngine.AI;
 
 public class MovimientoPuntos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Vector3[] listaVectores ;
-
+    public Vector3[] listaVectores = new Vector3[4];
     public NavMeshAgent agente;
-
-    int actual = 0;
-
+    public Transform destino;
+    static int actual = 0;
+   
+    // Start is called before the first frame update
     private void Start()
     {
-       /* TODO: Crear la lista de puntos (se puede hacer por código o desde la UI*/
-       /* TODO: Establecer el primer punto como destino */
+        /* TODO: Crear la lista de puntos (se puede hacer por código o desde la UI*/
+
+        /* TODO: Establecer el primer punto como destino */
+        agente = GetComponent<NavMeshAgent>();
+        agente.destination = listaVectores[actual];
 
     }
 
     // Update is called once per frame
     void Update()
     {
-       /* El algoritmo es algo así:
-        *  1 - Si hemos llegado al destino (es decir, la x y z de mi transform es igual al destino establecido en el agente)
-        *  2 - Busco cual es el siguiente punto, y lo establezco como destino. ¿Qué pasa si hemos llegado al último punto?
-        */
-      
+        /* El algoritmo es algo así:
+         *  1 - Si hemos llegado al destino (es decir, la x y z de mi transform es igual al destino establecido en el agente)
+         *  2 - Busco cual es el siguiente punto, y lo establezco como destino. ¿Qué pasa si hemos llegado al último punto?
+         */
+        // transform.position -> posicion personaje
+        // agent.destination -> destino
+        if (transform.position.x == agente.destination.x && transform.position.z == agente.destination.z)
+        {
+            actual++;
+            if (actual > 4)
+            {
+                actual = 0;
+            }
+            agente.destination = listaVectores[actual];
+        }
     }
 }
